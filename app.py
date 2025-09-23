0b9987758e37affb7a1f65ace7ae9565daf4ff94
raworiginal <randall.a.wolfe@gmail.com>
2025-09-23 08:39:16 -0700

    added CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from dotenv import load_dotenv
 from blueprints import auth_bp, topics_bp, lists_bp
 from proxy import api_bp
@@ -6,6 +7,7 @@ from proxy import api_bp
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(topics_bp)
 app.register_blueprint(lists_bp)
